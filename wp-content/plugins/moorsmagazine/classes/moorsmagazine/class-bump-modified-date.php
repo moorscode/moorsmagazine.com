f0ce32c7b2e9e00098b6912727e854a495b59f0c
Jip Moors <jhp.moors@gmail.com>
2017-11-09 18:11:57 +0100

    Bump wpseo sitemap date

--- wp-content/plugins/moorsmagazine/classes/moorsmagazine/class-bump-modified-date.php
+++ wp-content/plugins/moorsmagazine/classes/moorsmagazine/class-bump-modified-date.php
@@ -3,15 +3,46 @@
 namespace moorsmagazine;
 
 class Bump_Modified_Date {
+	protected static $timestamp = 1510056000; // mktime( 12, 0, 0, 11, 7, 2017 );
+
 	public function add_hooks() {
 		add_filter( 'get_the_modified_date', [ $this, 'overwrite_modified_date' ], 10, 3 );
+		add_filter( 'wpseo_sitemap_entry', [ $this, 'overwrite_sitemap_date' ], 10, 2 );
 	}
 
+	/**
+	 * Overwrites the modified date for the sitemap.
+	 *
+	 * @param array $url
+	 * @param string $post_type
+	 *
+	 * @return array
+	 */
+	public function overwrite_sitemap_date( $url, $post_type ) {
+		if ( $post_type === 'post' ) {
+			$timestamp = mysql2date( 'U', $url['mod'] );
+			if ( $timestamp < self::$timestamp ) {
+				$url['mod'] = gmdate( 'Y-m-d H:i:s', self::$timestamp );
+			}
+		}
+
+		return $url;
+	}
+
+	/**
+	 * Overwrites the last modified date.
+	 *
+	 * @param int $the_time
+	 * @param string $d
+	 * @param \WP_Post $post
+	 *
+	 * @return false|string
+	 */
 	public function overwrite_modified_date( $the_time, $d, $post ) {
 		$timestamp = mysql2date( 'U', $post->post_modified );
-		$test = mktime( 12, 0, 0, 11, 7, 2017 );
-		if ( $timestamp < $test ) {
-			return date( 'c', $test );
+
+		if ( $timestamp < self::$timestamp ) {
+			return date( 'c', self::$timestamp );
 		}
 
 		return $the_time;
